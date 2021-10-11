d24ca5b08197f530313b82e663d48ad03e55af14
Facundo Gorla <facundo.gorla@gmail.com>
2021-10-11 16:44:09 -0300

    Finished exercise 1

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+// a) Crear dos variables numéricas y utilizar el operador suma 
+// para guardar el valor de la suma de ambos números en una 3er variable.
+var aNum1 = 1
+var aNum2 = 2
+var aSum = aNum1 + aNum2
+console.log(aSum)
+
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var bString1 = "Radium"
+var bString2 = "Rocket"
+var bConcat = bString1 + bString2
+console.log(bConcat)
+// o
+var bConcat2 = bString1.concat(bString2)
+console.log(bConcat2)
+
+// c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+// guardando el resultado de la suma en una 3er variable (utilizar length).
+var cString1 = "Radium"
+var cString2 = "Rocket"
+var cSumLenght = cString1.length + cString2.length
+console.log(cSumLenght)
